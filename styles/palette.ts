92ee3c89c898b383c48532b264b78acdd79afb56
yurileeeee <yuriuniv@gmail.com>
2022-02-14 21:08:27 +0900

    Feat: Header 구성

---
+++ styles/palette.ts
@@ -0,0 +1,11 @@
+export default {
+  red: "#FFAFB0",
+  orang: "#FFC282",
+  yellow: "#FCFFB0",
+  green: "#E2FFAF",
+  blur: "#AEE4EF",
+  navy: "#B5C7ED",
+  gray: "#E5E5E5",
+  deep_red: "#F35456",
+  deep_green: "#47E774",
+};
