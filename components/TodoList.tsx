1db5f2fcc252a79b7ee2be2043183f75022bdd92
yurileeeee <yuriuniv@gmail.com>
2022-02-14 21:27:31 +0900

    Feat: 투두리스트 스타일링

---
+++ components/TodoList.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../styles/palette";
+import { TodoType } from "../types/todo";
+
+const Container = styled.div`
+  width: 100%;
+
+  .todo-list-header {
+    padding: 12px;
+    border-bottom: 1px solid ${palette.gray};
+
+    .todo-list-last-todo {
+      font-size: 14px;
+      span {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
+
+interface IProps {
+  todos: TodoType[];
+}
+
+const TodoList: React.FC<IProps> = ({ todos }) => {
+  return (
+    <Container>
+      <div className="todo-list-header">
+        <p className="todo-list-last-todo">
+          남은 TODO<span>{todos.length}개</span>
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+export default TodoList;
